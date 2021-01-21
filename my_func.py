dbb375c90e451dd314e16e384e8614970eccea16
halder <sascha.halder91@gmail.com>
2021-01-21 21:33:47 +0100

    Implement t statistic

--- my_func.py
+++ my_func.py
@@ -40,6 +40,20 @@ def z_statistic(data, estimate):
     mean, _, std, __ = descriptive_stats(data)
     return (SQRT(n) / std) * (mean - estimate)
 
+def t_statistic(sample1, sample2):
+    """
+    T Test: Compare to samples
+
+    T = (mu1 - mu2) / sqrt( (var1 / n) / (var2 / m) )
+    T ~ tN, where N = WS degrees of freedom
+    """
+    n, m = len(sample1), len(sample2)
+    mu1, var1, _1, _11 = descriptive_stats(sample1)
+    mu2, var2, _2, _22 = descriptive_stats(sample2)
+    nom = mu1 - mu2
+    denom = SQRT( (var1 / n) + (var2 / m) )
+    return nom / denom
+
 def ws_df(sample1, sample2):
     """
     Implementation of Welch-Satterthwaite to determine degrees of freedom for t-tests.
