c03752a73b015e4774c85e246dc595c49d63fc92
halder <sascha.halder91@gmail.com>
2021-01-21 21:22:57 +0100

    Add Welch-Satterthwaite implementation

--- my_func.py
+++ my_func.py
@@ -28,12 +28,29 @@ def descriptive_stats(data, unbiased=False):
 
 def z_score(data, estimate):
     """
+    Z Test: Compare sample mean to population mean
+    --------------------------------------------------
     Normalization to N(0,1)
     X ~ N(mu,sigma^2) -> Z = (X - mu) / sigma ~ N(0,1)
     n >= 30 by CLT
     """
     n = len(data)
     if n < 30:
-        print("n < 30; CLT does not apply.")
+        print("n < 30. CLT does not apply.")
     mean, _, std, __ = descriptive_stats(data)
     return (SQRT(n) / std) * (mean - estimate)
+
+def ws_df(sample1, sample2):
+    """
+    Implementation of Welch-Satterthwaite to determine degrees of freedom for t-tests.
+
+    df = [ (std1^2 / n) + (std2^2 / m) ]^2 / [ (std1^4 / n^2*(n-1)) + (std2^4 / m^2*(m-1)) ]
+    """
+    n, m = len(sample1), len(sample2)
+    _1, var1, _11, _111 = descriptive_stats(sample1)
+    _2, var2, _22, _222 = descriptive_stats(sample2)
+    nom = ((var1 / n) + (var2 / m))**2
+    denom = ((var1**2 / (n**2 * (n - 1))) + (var2**2 / (m**2 * (m - 1))))
+    N = nom / denom
+    # round down to be conservative
+    return N // 1
